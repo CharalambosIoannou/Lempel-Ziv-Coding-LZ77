23be0db368fa046cf89840790bf35abc69328df3
CharalambosIoannou <cioannou1997@gmail.com>
2019-01-29 01:25:50 +0000

    Second commit

--- attempt 2.py
+++ attempt 2.py
@@ -1,28 +1,47 @@
 import time
+def find_in_dict(buffer, dictionary):
+    shift = len(dictionary)
+    substring = ""
 
+    for character in buffer:
+        substring_tmp = substring + character
+        shift_tmp = dictionary.rfind(substring_tmp)
+
+        if shift_tmp < 0:
+            break
+
+        substring = substring_tmp
+        shift = shift_tmp
+
+    return len(substring), len(dictionary) - shift
 # Input inp
 #inp = "Peter Piper picked a peck of pickled peppers.A peck of pickled peppers Peter Piper picked.If Peter Piper picked a peck of pickled peppers,Where's the peck of pickled peppers Peter Piper picked"
-inp="ABRACADABRAD"
+inp="ABRACADABRA"
 # Get time
 delta0 = time.time()
 
-# BIG ENCODING LOOP STARTS WITH position #
 position = 0
 
 found_chars = []
-while position < len(inp):    
+while position < len(inp) :    
     look_ahead_buffer = 0
     temp = -1
-    count = -1
+    #count = -1
     found = False
     for i in range(position, len(inp)):
+
         count = inp.rfind(inp[position:position + look_ahead_buffer + 1], 0, position)
+        count1,count2 = find_in_dict(inp[position:position + look_ahead_buffer + 1],inp)
         print(count)
+        print(count1)
+        print(count2)
+        if count == -1:
+            break
         if count >= 0:
             look_ahead_buffer += 1
             temp = count
             found = True
-
+    
     if (found == False):
         next_letter= inp[position]
         print("(0,0," , inp[position],  ")")
@@ -35,19 +54,15 @@ while position < len(inp):
                 next_letter="-"
             else:
                 next_letter = inp[position + look_ahead_buffer]
-            #print ("{0:10}".format("(" + str(position - temp) + "," + str(look_ahead_buffer) + ")") + "{0:5}".format(end) )
-            #print("(",position-temp,",",look_ahead_buffer,",",next_letter,")")
             print("(",','.join([str(position - temp),str( look_ahead_buffer), next_letter ]),")")
             found_chars.append([position - temp, look_ahead_buffer, next_letter ])
             position = position+ look_ahead_buffer + 1            
             
         else:
-            print("here")
-            #print ("{0:10}".format("(" + str(position - temp) + "," + str(look_ahead_buffer - 1) + ")") + "{0:5}".format(inp[position + look_ahead_buffer - 1]) )
             print([position - temp, look_ahead_buffer , inp[position + look_ahead_buffer-1 ]])
             found_chars.append([position - temp, look_ahead_buffer , inp[position + look_ahead_buffer-1 ]])            
             break
-# BIG ENCODING LOOP ENDS HERE #
+
 
 
 delta1 = time.time()
---
+++ test.py
@@ -0,0 +1,49 @@
+def find(word,inp):
+    for position1,w in enumerate(inp):
+        if w == string_to_check:
+            total= position1 + 1
+            print(total )
+            return (total)
+        else:
+            return 0
+
+inp="ABRACADABRA"
+length_inp=len(inp)
+position=0
+found=[]
+dictionary=[]
+
+len_buffer=0
+counter=0
+while (position <=length_inp-1 ):
+    if (inp[position] not in found):
+        found.append(inp[position])
+        nextLett=inp[position]
+        position=position +1
+        d=0
+        length=0       
+        dictionary.append([0,0,nextLett])
+        print("(0,0,",nextLett,")") 
+    else:
+        print("old pos: ", position)
+        for i in range (position,length_inp):
+            string_to_check= count = inp.rfind(inp[position:position + 1], 0, position)
+            print(string_to_check) 
+            print("length of repeating: " , find(string_to_check,inp))
+            if (find(string_to_check,inp) == 0): 
+                string_to_check=string_to_check[:-1]
+                length_string_to_check=len(string_to_check)
+                nextLett=inp[position+1]
+                #print(string_to_check)
+                #print("pos: " , position)
+                count=inp.rfind(inp[position:position + 1], 0, position)               
+                print(position-count,",",length_string_to_check,",",nextLett)
+                position=position+length_string_to_check+1
+                print("new pos: ", position)
+                dictionary.append([position-count,",",length_string_to_check,",",nextLett])
+            else:
+                print(" else string : ", string_to_check) 
+                print(" else length of repeating: " , find(string_to_check,inp))
+                continue
+
+        
